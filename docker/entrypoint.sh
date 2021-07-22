54269cb8493fe1499efdd5ded62a12940bf71da9
Teeh Amaral <contato@teehamaral.me>
2021-07-22 17:29:02 -0300

    PHP-FPM tweaks

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,9 @@ case $1 in
         sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/8.0/fpm/php.ini
         sed -e 's/;daemonize = yes/daemonize = no/' -i /etc/php/8.0/fpm/php-fpm.conf
         sed -i -e "s/;clear_env = no/clear_env = no/g" /etc/php/8.0/fpm/pool.d/www.conf
+        sed -i -e "s/;pm.max_children = 5/pm.max_children = 500/g" /etc/php/8.0/fpm/pool.d/www.conf
+        sed -i -e "s/;pm.max_requests = 500/pm.max_requests = 2000/g" /etc/php/8.0/fpm/pool.d/www.conf
+        sed -i -e "s/;pm.max_spare_servers = 3/pm.max_requests = 25/g" /etc/php/8.0/fpm/pool.d/www.conf
         /etc/init.d/php8.0-fpm restart
         supervisord -n -c /var/www/html/docker/supervisord.conf
     ;;
